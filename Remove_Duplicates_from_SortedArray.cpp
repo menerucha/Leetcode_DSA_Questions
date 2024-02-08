6c811e6a51d2cf42f27eaaea6ddf4a8e2f5de596
Rucha Mene <130368638+menerucha@users.noreply.github.com>
2024-02-08 12:15:29 +0530

    Day_02

---
+++ Remove_Duplicates_from_SortedArray.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+class Solution
+{
+public:
+    int removeDuplicates(std::vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 0)
+        {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < n; j++)
+        {
+            if (nums[i] != nums[j])
+            {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+
+        return i + 1;
+    }
+};
