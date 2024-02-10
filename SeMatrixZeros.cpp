0dd5fa9a7d641d4544651754cc88ac7ec95eb46d
Rucha Mene <menerucha2004@gmail.com>
2024-02-10 10:16:56 +0530

    Your commit message

---
+++ SeMatrixZeros.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        if (n == 0)
+            return;
+
+        int m = matrix[0].size();
+        if (m == 0)
+            return;
+
+        vector<int> col(m, 0);
+        vector<int> row(n, 0);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (row[i] || col[j])
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
