953f8cca624086b69d6eda9368cc2a46cbb95472
Rucha Mene <130368638+menerucha@users.noreply.github.com>
2024-02-19 19:11:40 +0530

    DAY_62

---
+++ Power_of_two.cpp
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    { // Since C++20
+        return n > 0 && countl_zero((unsigned)n) + countr_zero((unsigned)n) == 31;
+    }
+=======
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    { // Since C++20
+        return n > 0 && countl_zero((unsigned)n) + countr_zero((unsigned)n) == 31;
+    }
+>>>>>>> 09e55041003f5eaf0f7cdd521aa3af4aee03d086
+};
