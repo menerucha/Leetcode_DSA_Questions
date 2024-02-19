2d0ea982adf945ba75462635b82cccabdccf6792
Rucha Mene <130368638+menerucha@users.noreply.github.com>
2024-02-19 11:13:45 +0530

    DAY_47

---
+++ InsertionSortlist.cpp
@@ -0,0 +1,94 @@
+<<<<<<< HEAD
+class Solution
+{
+public:
+    ListNode *insertionSortList(ListNode *head)
+    {
+        ListNode *ptr = head;
+        ListNode *nh = NULL;
+        ListNode *f = NULL;
+        while (ptr != NULL)
+        {
+            f = ptr->next;
+            ptr->next = NULL;
+            if (nh == NULL)
+            {
+                nh = ptr;
+            }
+            else
+            {
+                if (nh->val > ptr->val)
+                {
+                    ptr->next = nh;
+                    nh = ptr;
+                }
+                else
+                {
+                    ListNode *tem = nh;
+                    while (tem->next != NULL && tem->next->val < ptr->val)
+                    {
+                        tem = tem->next;
+                    }
+                    if (tem->next != NULL)
+                    {
+                        ptr->next = tem->next;
+                        tem->next = ptr;
+                    }
+                    else
+                    {
+                        tem->next = ptr;
+                    }
+                }
+            }
+            ptr = f;
+        }
+        return nh;
+    }
+=======
+class Solution
+{
+public:
+    ListNode *insertionSortList(ListNode *head)
+    {
+        ListNode *ptr = head;
+        ListNode *nh = NULL;
+        ListNode *f = NULL;
+        while (ptr != NULL)
+        {
+            f = ptr->next;
+            ptr->next = NULL;
+            if (nh == NULL)
+            {
+                nh = ptr;
+            }
+            else
+            {
+                if (nh->val > ptr->val)
+                {
+                    ptr->next = nh;
+                    nh = ptr;
+                }
+                else
+                {
+                    ListNode *tem = nh;
+                    while (tem->next != NULL && tem->next->val < ptr->val)
+                    {
+                        tem = tem->next;
+                    }
+                    if (tem->next != NULL)
+                    {
+                        ptr->next = tem->next;
+                        tem->next = ptr;
+                    }
+                    else
+                    {
+                        tem->next = ptr;
+                    }
+                }
+            }
+            ptr = f;
+        }
+        return nh;
+    }
+>>>>>>> 65a4e95468639fd8339b68a80f54ff05428a8998
+};
