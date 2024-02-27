d9e039ba3e486ec6b9f0989562404a34c7d21660
menerucha <menerucha2004@gmail.com>
2024-02-27 11:18:52 +0530

    DAY_104

---
+++ Remove_linked_list_elements.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    ListNode *removeElements(ListNode *head, int val)
+    {
+        struct ListNode *temp = (struct ListNode *)malloc(sizeof(struct ListNode));
+        temp->next = head;
+        struct ListNode *curr = temp;
+        while (curr->next != NULL)
+        {
+            if (curr->next->val == val)
+                curr->next = curr->next->next;
+            else
+                curr = curr->next;
+        }
+        return temp->next;
+    }
+};
