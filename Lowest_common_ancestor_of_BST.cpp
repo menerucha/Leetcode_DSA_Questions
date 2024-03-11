98d127fe4a6f39f46f4df8287909dd2eeafff36e
menerucha <menerucha2004@gmail.com>
2024-03-11 14:58:24 +0530

    DAY_167

---
+++ Lowest_common_ancestor_of_BST.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        while (root != NULL)
+        {
+            if (p->val < root->val && q->val < root->val)
+            {
+                root = root->left;
+            }
+            else if (p->val > root->val && q->val > root->val)
+            {
+                root = root->right;
+            }
+            else
+            {
+                return root;
+            }
+        }
+        return NULL;
+    }
+};
