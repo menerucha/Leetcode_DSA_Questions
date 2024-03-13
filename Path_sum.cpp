d50c035a3cc83b9129d7bafe40e7c9f70aa41eb0
menerucha <menerucha2004@gmail.com>
2024-03-13 22:36:15 +0530

    DAY_183

---
+++ Path_sum.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    bool hasPathSum(TreeNode *root, int targetSum)
+    {
+        if (root == NULL)
+        {
+            return false;
+        }
+        if (root->left == NULL && root->right == NULL)
+        {
+            return root->val == targetSum;
+        }
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
+    }
+};
