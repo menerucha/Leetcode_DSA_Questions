de2a90ab69fd1a82e5eef86ec76f35737d25a5c1
menerucha <menerucha2004@gmail.com>
2024-03-15 23:56:41 +0530

    DAY_194

---
+++ Partition_list.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    ListNode *partition(ListNode *head, int x)
+    {
+        ListNode *dummy1 = new ListNode();
+        ListNode *temp1 = dummy1;
+        ListNode *dummy2 = new ListNode();
+        ListNode *temp2 = dummy2;
+        while (head != NULL)
+        {
+            if (head->val < x)
+            {
+                temp1->next = head;
+                temp1 = temp1->next;
+            }
+            else
+            {
+                temp2->next = head;
+                temp2 = temp2->next;
+            }
+            head = head->next;
+        }
+        if (dummy1->next == NULL)
+            return dummy2->next;
+        temp1->next = dummy2->next;
+        temp2->next = NULL;
+        return dummy1->next;
+    }
+};
