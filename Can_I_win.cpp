8b71cfd63a147929ad0d6408b819be7eb8eeefbe
menerucha <menerucha2004@gmail.com>
2024-03-15 23:44:46 +0530

    DAY_193

---
+++ Can_I_win.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+private:
+    int m, t;
+    vector<int> dp;
+    bool game(int mask, int score)
+    {
+        if (dp[mask] != -1)
+            return dp[mask];
+        for (int i = 0; i < m; i++)
+        {
+            int cmask = 1 << i;
+            if ((mask & cmask) == 0)
+                if (score + i + 1 >= t or game((mask | cmask), score + i + 1) == false)
+                    return dp[mask] = 1;
+        }
+        return dp[mask] = 0;
+    }
+
+public:
+    bool canIWin(int maxChoosableInteger, int desiredTotal)
+    {
+        m = maxChoosableInteger;
+        t = desiredTotal;
+        if ((m * (m + 1)) / 2 < t)
+            return false;
+        dp.resize(1 << m, -1);
+        return game(0, 0);
+    }
+};
