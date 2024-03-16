90e3c8efcff2c9134dd699f02646e82c475c9d60
menerucha <menerucha2004@gmail.com>
2024-03-16 22:55:31 +0530

    DAY_196

---
+++ Reverse_Bits.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    uint32_t reverseBits(uint32_t n)
+    {
+        unsigned int ans = 0;
+        for (int i = 0; i <= 31; i++)
+        {
+            ans = (ans << 1) | ((n >> i) & 1);
+        }
+        return ans;
+    }
+};
