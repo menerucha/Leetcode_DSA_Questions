e945df71534e4a06157b7d379053168cbc94f171
menerucha <menerucha2004@gmail.com>
2024-03-18 21:34:53 +0530

    DAY_207

---
+++ Ransom_note.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        unordered_map<char, int> dictionary;
+        for (char c : magazine)
+        {
+            if (dictionary.find(c) == dictionary.end())
+            {
+                dictionary[c] = 1;
+            }
+            else
+            {
+                dictionary[c]++;
+            }
+        }
+        for (char c : ransomNote)
+        {
+            if (dictionary.find(c) != dictionary.end() && dictionary[c] > 0)
+            {
+                dictionary[c]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
