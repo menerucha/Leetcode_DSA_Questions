650df1834bf3620d8044e4a476048d90e0a04b49
menerucha <menerucha2004@gmail.com>
2024-03-25 11:13:56 +0530

    DAY_210

---
+++ first_missing_positive.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0 && nums[i] <= nums.size())
+            {
+                if (nums[i] != i + 1)
+                {
+                    if (nums[nums[i] - 1] != nums[i])
+                    {
+                        swap(nums[i], nums[nums[i] - 1]);
+                        i--;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+        return nums.size() + 1;
+    }
+};
